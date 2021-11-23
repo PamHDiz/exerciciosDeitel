34a24b695c407d242f5642ea338acc980a97aaf8
PamHDiz <pamhdiz@gmail.com>
2021-11-23 10:27:50 -0300

    Utilizando Array para mostrar frequência de ocorrência.

---
+++ exerciciosDeitel/src/br/com/deitel/exercicios/BarraDeAsterisco.java
@@ -0,0 +1,27 @@
+package br.com.deitel.exercicios;
+
+public class BarraDeAsterisco {
+
+	public static void main(String[] args) {
+		
+		int[] array = {0, 0, 0, 0, 0, 0, 1, 2, 4, 3, 1};
+		
+		System.out.println("Grade de distribuição:");
+		
+		for(int i = 0; i < array.length; i++) {
+			
+			if(i == 10) {
+				System.out.printf("%5d: ", 100);
+			} else {
+				System.out.printf("%02d-%02d: ", i * 10, i * 10 + 9);
+			}
+			
+			for(int asterisco = 0; asterisco < array[i]; asterisco++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
