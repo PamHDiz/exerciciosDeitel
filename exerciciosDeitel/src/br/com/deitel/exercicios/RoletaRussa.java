b5ec590db9343670cab89312f43047af1102629c
PamHDiz <pamhdiz@gmail.com>
2021-11-23 10:25:03 -0300

    Aprimorando o jogo de dados utilizando Array em vez de Switch.

--- exerciciosDeitel/src/br/com/deitel/exercicios/RoletaRussa.java
+++ exerciciosDeitel/src/br/com/deitel/exercicios/RoletaRussa.java
@@ -1,14 +1,28 @@
 package br.com.deitel.exercicios;
 
 import java.security.SecureRandom;
+//Programa utilizando Arrays ao inves de switch
 
 public class RoletaRussa {
 
 	public static void main(String[] args) {
 
 		SecureRandom numeroRandom = new SecureRandom();
-
-		int frequencia1 = 0;
+		int[] frequencia = new int[7]; // array para contar a frenquencia
+		
+		//para lançar o dado 6.000.000 de vezes e usa o valor do dado como indice da frequencia
+		for(int rolar = 1; rolar <= 6000000; rolar++) {
+			++frequencia[1 + numeroRandom.nextInt(6)];
+		}
+		
+		System.out.printf("%s%12s%n", "Face", "Frequencia");
+		
+		// gera saída do valor de cada elemento do Array
+		for(int face = 1; face < frequencia.length; face++) {
+			System.out.printf("%4d%10d%n", face, frequencia[face]);
+		}
+		
+		/*int frequencia1 = 0;
 		int frequencia2 = 0;
 		int frequencia3 = 0;
 		int frequencia4 = 0;
@@ -49,6 +63,8 @@ public class RoletaRussa {
 		System.out.printf("1\t%d%n2\t%d%n3\t%d%n4\t%d%n5\t%d%n6\t%d%n", 
 				frequencia1, frequencia2, frequencia3, frequencia4, 
 				frequencia5, frequencia6);
+	*/
+		
+		
 	}
-
 }
